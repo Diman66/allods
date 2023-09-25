e222eb24722edbe89c26bff60beb525d1ba4d5e9
Diman66 <pulnikov66@yandex.ru>
2023-09-25 21:03:59 +0500

    1

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}) // add options if needed
+      ],
+    }
+  }
+})
